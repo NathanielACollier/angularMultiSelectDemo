e093e2962a8226d074c4b75deb638ef630392380
Nathaniel Collier <drxs3v3n@gmail.com>
2020-10-07 15:19:25 -0500

    Pulling in angular
    Going to use angular to try out the multiselect.
    
    Need to figure out how to serve the angular files from express

--- serverSrc/index.ts
+++ serverSrc/index.ts
@@ -9,6 +9,9 @@ app.options('*', cors());
 
 app.use('/general', generalController);
 
+// serve angular
+app.use('/', express.static('../dist'));
+
 app.listen(port, () => {
     console.log(`App listening on port: ${port}`);
 });
