2ebf74577e6458938e5e0547e7357f71a9e7f97b
Nathaniel Collier <drxs3v3n@gmail.com>
2020-10-08 11:52:13 -0500

    Setup to serve the angular app

--- serverSrc/index.ts
+++ serverSrc/index.ts
@@ -3,6 +3,11 @@ import express = require('express');
 const cors = require('cors');
 import { generalController } from './controllers/general';
 
+
+function serveIndex(request:any,response:any){
+    response.sendFile(path.resolve('./dist/Index.html'));
+}
+
 let port = 8080;
 const app: express.Application = express();
 app.use(cors());
@@ -11,7 +16,11 @@ app.options('*', cors());
 app.use('/general', generalController);
 
 // serve angular
-app.use('/', express.static(path.resolve( '../dist')));
+// followed the way this person does it: https://stackoverflow.com/questions/39845526/how-to-serve-an-angular2-app-in-a-node-js-server
+app.use( express.static(path.resolve( './dist')));
+app.get('/', serveIndex);
+app.get('/*', serveIndex);
+
 
 app.listen(port, () => {
     console.log(`App listening on port: ${port}`);
